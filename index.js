7944060d2e5ac8e954b2c5e66f390c41597273f2
Jhovan771 <strangebanning@gmail.com>
2024-02-28 00:21:52 +0800

    ffdfsdf

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql2.createPool({
   host: "sql.freedb.tech",
   user: "freedb_hueco",
   password: "?WmvH3c$&E8TsUZ",
-  database: "thesis2_db",
+  database: "ThesisDB",
 });
 
 app.use(cors());
